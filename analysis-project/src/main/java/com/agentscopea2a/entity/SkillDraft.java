5075cba9d6738236d6b68fa87119ada9a37dac74
dengshuaifeng <101219253@qq.com>
2026-07-24 09:56:40 +0800

    feat: add 6 entities for publish/approval/draft/version/op-history/disable

---
+++ analysis-project/src/main/java/com/agentscopea2a/entity/SkillDraft.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2024-2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.agentscopea2a.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * Skill 草稿实体，保存技能编辑过程中的暂存内容与提交/审批时间线。
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SkillDraft {
+    private Long id;
+    private Long skillId;
+    private String name;
+    private String description;
+    private String content;
+    private String category;
+    private String tags;
+    private String status;
+    private String submitter;
+    private String approver;
+    private String approveComment;
+    private LocalDateTime submittedAt;
+    private LocalDateTime approvedAt;
+}
---
+++ analysis-project/src/main/java/com/agentscopea2a/entity/SkillPublish.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2024-2026 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.agentscopea2a.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * Skill 发布记录实体，记录技能向不同目标的发布流程与审批状态。
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SkillPublish {
+    private Long id;
+    private Long skillId;
+    private String targetType;
+    private String targetId;
+    private String targetName;
+    private String status;
+    private String submitter;
+    private String approver;
+    private LocalDateTime approveTime;
+    private String currentApproverUserId;
+    private String lastApprovalComment;
+    private LocalDateTime lastApprovalAt;
+    private LocalDateTime createdAt;
+}
